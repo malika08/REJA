7659409250fce7cde4596f6c9081eac6e258f92b
malika_south.k <malikaatoeva96@gmail.com>
2024-05-19 10:17:02 +0900

    feat: trainjs  callback and asynch

--- train.js
+++ train.js
@@ -9,7 +9,7 @@ const list = [
   "yoshlarga investitsiya qiling", //50-60
   "endi dam oling, foydasi yo'q endi", // 60
 ];
-
+/*CAll Back function
 function maslahatBering(a, callback) {
   if (typeof a !== "number") callback("insert a number", null);
   else if (a <= 20) callback(null, list[0]);
@@ -34,4 +34,45 @@ maslahatBering(66, (err, data) => {
     console.log("javob:", data);
   }
 });
+console.log(`passed here 1`);
+*/
+async function maslahatBering(a) {
+  if (typeof a !== "number") throw new Error("insert a number");
+  else if (a <= 20) return list[0];
+  else if (a > 20 && a <= 30) return list[1];
+  else if (a > 30 && a <= 40) return list[2];
+  else if (a > 40 && a <= 50) return list[3];
+  else if (a > 50 && a <= 60) return list[4];
+  // else {
+  //   return list[5];
+  // }
+  else {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(list[5]);
+      }, 1000);
+    });
+  }
+}
+console.log("passed here 0");
+
+/* then and catch yo'li orqali call qilish 
+maslahatBering(25)
+  .then((data) => {
+    console.log("javob:", data);
+  })
+
+  .catch((err) => {
+    console.log("Error:", err);
+  }); */
+// call sectionda asyn function ishlatish
+async function run() {
+  let javob = await maslahatBering(65);
+  console.log(javob);
+  javob = await maslahatBering(31);
+  console.log(javob);
+  javob = await maslahatBering(41);
+  console.log(javob);
+}
+run();
 console.log(`passed here 1`);
